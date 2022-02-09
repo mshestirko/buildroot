9b9be676fd9b6a977468b36d82dc26fe5dc0ee75
Maksym <maksym.shestirko@vizio.com>
2022-02-08 23:02:12 -0600

    Basic changes for support QT UI of IR commutator.

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -9,3 +9,10 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
+
+cp package/busybox/S10mdev ${TARGET_DIR}/etc/init.d/S10mdev
+chmod 755 ${TARGET_DIR}/etc/init.d/S10mdev
+cp package/busybox/mdev.conf ${TARGET_DIR}/etc/mdev.conf
+cp board/raspberrypi3/interfaces ${TARGET_DIR}/etc/network/interfaces
+cp board/raspberrypi3/wpa_supplicant.conf ${TARGET_DIR}/etc/wpa_supplicant.conf
+cp board/raspberrypi/sshd_config  ${TARGET_DIR}/etc/ssh/sshd_config
